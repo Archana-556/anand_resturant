cc67435e869e8da84b7361ecd6fa57ec0effc2cb
Archana Anand <myarchu566@gmail.com>
2025-08-15 14:37:58 +0530

    Initial commit

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,35 @@
-const express= require ('express');
+const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const path =require('path');
-require ('dotenv').config();
+const path = require('path');
+require('dotenv').config();
+
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+// API Routes
 const menuRoutes = require('./routes/menuRoutes');
 app.use('/api/menuItem', menuRoutes);
 
+// Serve static files from ../public
+app.use(express.static(path.join(__dirname, '../public')));
 
-app.use(express.static(path.join(__dirname , 'public')));
-app.get('/', (req, res)=>{
-  res.sendFile(__dirname, 'public', 'menuForm.html');
+// Default route (homepage)
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public', 'index.html'));
 });
 
+// Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log("MongoDB connected!");
-    app.listen(process.env.PORT, () =>
-      console.log(`Server running on http://localhost:${process.env.PORT}`)
-    );
-  })
+  .then(() => console.log("MongoDB connected!"))
   .catch(err => console.log(err));
+
+// Export app for Vercel
+module.exports = app;
+
+// If running locally, start server
+if (require.main === module) {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+}
